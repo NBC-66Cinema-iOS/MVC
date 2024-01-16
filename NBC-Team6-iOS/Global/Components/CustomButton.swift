49d1d2e21e011520cd79fa4c794898135d7bbb7d
hay-yo <91271462+hay-yo@users.noreply.github.com>
2024-01-16 20:41:04 +0900

    sign-in & sign-up UI

--- NBC-Team6-iOS/Global/Components/CustomButton.swift
+++ NBC-Team6-iOS/Global/Components/CustomButton.swift
@@ -5,4 +5,27 @@
 //  Created by Joon Baek on 2024/01/16.
 //
 
-import Foundation
+import UIKit
+
+class CustomButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontColor: UIColor, backColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        configuration = .filled()
+        configuration?.title = title
+        configuration?.baseForegroundColor = fontColor
+        configuration?.baseBackgroundColor = backColor
+        configuration?.cornerStyle = .medium
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
--- NBC-Team6-iOS/Presentation/SignUp/SignUpViewController.swift
+++ NBC-Team6-iOS/Presentation/SignUp/SignUpViewController.swift
@@ -9,21 +9,102 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    // MARK: - Property
+    let introLabel = UILabel()
+    let iconImageView = UIImageView()
+    let idTextField = UITextField()
+    let passwordTextField = UITextField()
+    let signupButton = CustomButton(fontColor: .white, backColor: .black, title: "회원가입")
+    
+    // UserDefaults
+    //let memberUserDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Layout 
+    // configurations
+    func configureUI() {
+        configureIconImageView()
+        configureIntroLable()
+        configureIdTextField()
+        configureSignUpButton()
+        configurePasswordTextField()
     }
-    */
+    
+    // 아이콘
+    func configureIconImageView() {
+        view.addSubview(iconImageView)
+        iconImageView.image = UIImage(systemName: "film.stack")
+        iconImageView.contentMode = .center
+        iconImageView.tintColor = .black
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            iconImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            iconImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            iconImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
+            //iconImageView.heightAnchor.constraint(equalToConstant: 100) -> image 넣을때 heightAnchor 설정해줘야 함
+        ])
+    }
+    
+    // 인트로 레이블
+    func configureIntroLable() {
+        view.addSubview(introLabel)
+        introLabel.text = "MOVIE66\n회원이 되어보세요!"
+        introLabel.numberOfLines = 0
+        introLabel.textAlignment = .center
+        introLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            introLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 10),
+            introLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            introLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
+        ])
+    }
+    
+    // 아이디 입력
+    func configureIdTextField() {
+        view.addSubview(idTextField)
+        idTextField.placeholder = "아이디 입력"
+        idTextField.borderStyle = .roundedRect
+        idTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            idTextField.topAnchor.constraint(equalTo: introLabel.bottomAnchor, constant: 60),
+            idTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            idTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
+        ])
+    }
+    
+    // 비밀번호 입력
+    func configurePasswordTextField() {
+        view.addSubview(passwordTextField)
+        passwordTextField.placeholder = "비밀번호 입력"
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
 
+        NSLayoutConstraint.activate([
+            passwordTextField.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 10),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+   
+    // 회원가입 버튼
+    func configureSignUpButton() {
+        view.addSubview(signupButton)
+        //signupButton.addTarget(self, action: #selector(toSignupView), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            signupButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 420),
+            signupButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            signupButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
 }
