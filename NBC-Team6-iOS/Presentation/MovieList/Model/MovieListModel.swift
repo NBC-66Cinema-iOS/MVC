e18b145008066894405dd04d805d1264697e9656
joonBaek12 <joonbaek12@syuin.ac.kr>
2024-01-19 09:39:05 +0900

    [Feat] #5 - MovieListView UI 구현

---
+++ NBC-Team6-iOS/Presentation/MovieList/Model/MovieListModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListModel.swift
+//  NBC-Team6-iOS
+//
+//  Created by Joon Baek on 2024/01/18.
+//
+
+import UIKit
+
+struct MovieListModel {
+    let image: UIImage
+    let title: String
+}
