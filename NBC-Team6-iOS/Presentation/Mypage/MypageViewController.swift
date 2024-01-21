46b6af48b92f2001a4e28418cfd03b63485ed7d2
hay-yo <91271462+hay-yo@users.noreply.github.com>
2024-01-21 21:13:41 +0900

    [feat] #9 - 필요없는 코멘트 삭제

--- NBC-Team6-iOS/Presentation/Mypage/MypageViewController.swift
+++ NBC-Team6-iOS/Presentation/Mypage/MypageViewController.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class MypageViewController: UIViewController {
-    // MARK: - Property
     
+    // MARK: - Property
     // ID 레이블
     let mypageIdLabel = UILabel()
     // ID 텍스트 -> UserDefaults에서 ID 정보 가져오기
     let mypageIdText = UILabel()
     // 예매내역 타이틀 레이블
     let mypageReservationLabel = UILabel()
-    // 예매내역 보여주기 -> ReservationViewController 에서 정보 가져오기
-    
     // 로그아웃 버튼
     let mypageLogoutButton = CustomButton(fontColor: .white, backColor: .black, title: "로그아웃")
     // UserDefaults
@@ -43,10 +41,9 @@ class MypageViewController: UIViewController {
     // MARK: - @objc
     // 로그아웃 버튼 눌렀을때 로그인 화면으로 가기 + 아이디랑 비밀번호 채워진것 없어지게 하기
     @objc func mypageLogoutButtonTapped() {
-        print("로그아웃됨")
         memberUserDefaults.removeObject(forKey: "userId")
         memberUserDefaults.removeObject(forKey: "password")
-
+        
         //스택 비우고 로그인 화면으로 가기
         navigationController?.setViewControllers([SignInViewController()], animated: true)
     }
@@ -105,7 +102,7 @@ class MypageViewController: UIViewController {
             mypageReservationLabel.topAnchor.constraint(equalTo: mypageIdText.bottomAnchor, constant: 50),
             mypageReservationLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
             mypageReservationLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
-            ])
+        ])
     }
     
     func configureMypageLogoutButton() {
